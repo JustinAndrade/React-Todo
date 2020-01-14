9035ceaefe98a431fc270119ccc6929af43aba1f
JustinAndrade <you@example.com>
2020-01-14 06:32:58 -0500

    Array data added to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+const arr = [
+  {
+    task: 'Build a React App',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Build a React App',
+    id: Date.now() + 1,
+    completed: false
+  },
+  {
+    task: 'Build a React App',
+    id: Date.now() + 2,
+    completed: false
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todoList: arr
+    }
+  }
   render() {
     return (
       <div>
